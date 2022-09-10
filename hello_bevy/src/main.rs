8712b6886f2d2b93a3dd8a358692eb8bb40ba09e
Joseph LeBlanc <joseph.t.leblanc@protonmail.com>
2022-09-09 19:19:44 -0700

    initialize workspace and configure hello_bevy Cargo.toml for bevy

---
+++ hello_bevy/src/main.rs
@@ -0,0 +1,40 @@
+use bevy::prelude::*;
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+fn add_people(mut commands: Commands) {
+    commands
+        .spawn()
+        .insert(Person)
+        .insert(Name("Elaina Proctor".to_string()));
+    commands
+        .spawn()
+        .insert(Person)
+        .insert(Name("Rick Rogers".to_string()));
+    commands
+        .spawn()
+        .insert(Person)
+        .insert(Name("Lana Felding".to_string()));
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in query.iter() {
+        println!("hello {}!", name.0);
+    }
+}
+
+fn hello_world() {
+    println!("hello world!");
+}
+
+fn main() {
+    App::new()
+        .add_startup_system(add_people)
+        .add_system(hello_world)
+        .add_system(greet_people)
+        .run();
+}
